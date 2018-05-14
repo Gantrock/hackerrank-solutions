5871c54f9073dbef682b7b49f1deed0913392351
Gantrock <Gantrock@users.noreply.github.com>
2018-05-14 15:52:17 -0700

    Update LilysHomework.java
    Added comments.

--- LilysHomework.java
+++ LilysHomework.java
@@ -9,22 +9,30 @@ import java.util.regex.*;
 public class Solution {
 
     static int lilysHomework(int[] arr) {
+        //The count of how many swaps we performed.
         int count = 0;
+        //If we need to swap values this variable will contain the index for the second value.
         int index = 0;
         
         //Key is the value from arr and the Value is the index of the value in arr
         HashMap<Integer, Integer> map = new HashMap<>();
+        //add every element from the input array into the map.
         for(int i = 0; i < arr.length; i++) {
             map.put(arr[i], i);
         }
         
+        //Make a shallow copy of the initial array.
         int[] sorted = Arrays.copyOf(arr, arr.length);
+        //Sort the sorted array
         Arrays.sort(sorted);        
         /*we iterate n times through the input list and compare it to a presorted version of said input. If the element at index i isn't correct we find it's position from the sorted version and swap. We also increment the count indicating how many swaps we've performed.*/
         for(int i = 0; i < arr.length; i++) {
             if(sorted[i] != arr[i]) {
+                //increment the number of swaps we made
                 count++;
+                //Find the index in the map for the value we want to swap with the value at i
                 index = map.get(sorted[i]);
+                //Replace the index of the current value in the map.
                 map.replace(arr[i], index);
                 swap(i, index, arr);
             }
@@ -32,7 +40,8 @@ public class Solution {
 
         return count;
     }
-
+    
+    //Perform a basic array swap with two indexes and the array.
     static void swap(int indOne, int indTwo, int[] arr) {
         int temp = arr[indOne];
         arr[indOne] = arr[indTwo];
@@ -47,6 +56,7 @@ public class Solution {
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         int[] arr = new int[n];
+        //An array that will contain all of the values of arr but will eventually be in reverse order.
         int[] rev = new int[n];
 
         String[] arrItems = scanner.nextLine().split(" ");
@@ -57,14 +67,14 @@ public class Solution {
             arr[i] = arrItem;
             rev[i] = arrItem;
         }
-        
-        int temp = 0;
+        //Reverse the order of the rev array
         for(int r = 0; r < n / 2; r++) {
-            temp = rev[r];
-            rev[r] = rev[(n-1)-r];
-            rev[(n-1)-r] = temp;
+            int temp = arr[r];
+            arr[r] = arr[(n - 1) - r];
+            arr[(n - 1) - r] = temp;
         }
-
+        
+        //result is the minimum value between performing the lilysHomework method on the original and reversed arrays.
         int result = Math.min(lilysHomework(arr), lilysHomework(rev));
 
         bufferedWriter.write(String.valueOf(result));
